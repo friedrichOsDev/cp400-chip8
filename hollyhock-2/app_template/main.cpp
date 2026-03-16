27af0f56a60febe913f39e95278b31c2d7d84e64
friedrichOsDev <f39597054@gmail.com>
2026-03-16 13:34:04 +0100

    add chip8 init

--- hollyhock-2/app_template/main.cpp
+++ hollyhock-2/app_template/main.cpp
@@ -1,7 +1,8 @@
 #include <appdef.hpp>
 #include <sdk/calc/calc.hpp>
 #include <sdk/os/lcd.hpp>
-#include <sdk/os/debug.hpp>
+#include <sdk/os/mem.hpp>
+#include "main.hpp"
 
 APP_NAME("CHIP-8 Emulator")
 APP_DESCRIPTION("A simple CHIP-8 Emulator")
@@ -12,4 +13,20 @@ extern "C"
 void main() {
 	calcInit(); // backup screen and init some variables
 	calcEnd(); // restore screen and do stuff
+}
+
+extern "C"
+void initChip8() {
+  chip8_hardware_t hw;
+  memset(&hw, 0, sizeof(chip8_hardware_t));
+  
+  hw.mem = malloc(4096);
+  hw.mem_size = 4096;
+  memset(hw.mem, 0, hw.men_size);
+  
+  hw.display = malloc(32 * 64);
+  hw.display_size = 32 * 64;
+  memset(hw.display, 0, hw.display_size);
+  
+  hw.pc = 0x200;
 }
