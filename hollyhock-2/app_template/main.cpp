789294b11666e955eba764fdd88804c0913dc651
friedrichOsDev <f39597054@gmail.com>
2026-03-17 06:15:37 +0000

    add Chip8 class and first ROM loading dialog

--- hollyhock-2/app_template/main.cpp
+++ hollyhock-2/app_template/main.cpp
@@ -2,6 +2,9 @@
 #include <sdk/calc/calc.hpp>
 #include <sdk/os/lcd.hpp>
 #include <sdk/os/mem.hpp>
+#include <sdk/os/file.hpp>
+#include <sdk/os/string.hpp>
+#include <sdk/os/debug.hpp>
 #include "main.hpp"
 
 APP_NAME("CHIP-8 Emulator")
@@ -9,24 +12,116 @@ APP_DESCRIPTION("A simple CHIP-8 Emulator")
 APP_AUTHOR("FriedrichOsDev")
 APP_VERSION("1.0.0")
 
-extern "C"
-void main() {
-	calcInit(); // backup screen and init some variables
-	calcEnd(); // restore screen and do stuff
+Chip8::Chip8() {
+	for (int i = 0; i < 16; i++) {
+		v[i] = 0;
+	}
+	
+	mem = (uint8_t *)malloc(4096);
+	mem_size = 4096;
+	memset(mem, 0, mem_size);
+	
+	display = (uint8_t *)malloc(64 * 32);
+	display_size = 64 * 32;
+	memset(display, 0, display_size);
+
+	for (int i = 0; i < 16; i++) {
+		stack[i] = 0;
+	}
+	
+	sp = 0;
+	i = 0;
+	pc = 0x200;
+	delay_timer = 0;
+}
+
+void Chip8::loadROM(const char * filename) {
+	(void)filename;
+}
+
+void Chip8::cycle() {
+	fetch();
+	decode();
+	execute();
+}
+
+void Chip8::fetch() {
+
+}
+
+void Chip8::decode() {
+
+}
+
+void Chip8::execute() {
+
 }
 
 extern "C"
-void initChip8() {
-  chip8_hardware_t hw;
-  memset(&hw, 0, sizeof(chip8_hardware_t));
-  
-  hw.mem = malloc(4096);
-  hw.mem_size = 4096;
-  memset(hw.mem, 0, hw.mem_size);
-  
-  hw.display = malloc(32 * 64);
-  hw.display_size = 32 * 64;
-  memset(hw.display, 0, hw.display_size);
-  
-  hw.pc = 0x200;
+void main() {
+	calcInit(); // backup screen and init some variables
+
+	// load ROM Dialog
+	GUIDialog loadROMDialog(
+		GUIDialog::Height35,
+		GUIDialog::AlignCenter,
+		"Select ROM",
+		GUIDialog::KeyboardStateNone
+	);
+
+	const uint16_t ROM_MENU_EVENT_ID = 1;
+	GUIDropDownMenu romMenu(
+		loadROMDialog.GetLeftX() + 10,
+		loadROMDialog.GetTopY() + 10,
+		loadROMDialog.GetRightX() - 10,
+		loadROMDialog.GetBottomY() - 10,
+		ROM_MENU_EVENT_ID
+	);
+
+	const uint16_t LOAD_BTN_EVENT_ID = GUIDialog::DialogResultOK;
+	GUIButton loadBtn(
+		loadROMDialog.GetLeftX() + 10,
+		loadROMDialog.GetTopY() + 45,
+		loadROMDialog.GetLeftX() + 10 + 100,
+		loadROMDialog.GetBottomY() - 10,
+		"Load",
+		LOAD_BTN_EVENT_ID
+	);
+
+	const uint16_t CANCEL_BTN_EVENT_ID = GUIDialog::DialogResultCancel;
+	GUIButton cancelBtn(
+		loadROMDialog.GetRightX() - 10 - 100,
+		loadROMDialog.GetTopY() + 45,
+		loadROMDialog.GetRightX() - 10,
+		loadROMDialog.GetBottomY() - 10,
+		"Cancel",
+		CANCEL_BTN_EVENT_ID
+	);
+
+	loadROMDialog.AddElement(romMenu);
+	loadROMDialog.AddElement(loadBtn);
+	loadROMDialog.AddElement(cancelBtn);
+
+	GUIDialog::DialogResult result = loadROMDialog.ShowDialog();
+
+	if (result != GUIDialog::DialogResultOK) {
+		calcEnd();
+		return;
+	}
+
+	Debug_Printf(10, 10, true, 0, "Selected Load Btn!");
+	LCD_Refresh();
+
+	Chip8 chip8;
+
+	while (true) {
+		chip8.cycle();
+		uint32_t key1, key2;
+		getKey(&key1, &key2);
+		if (testKey(key1, key2, KEY_CLEAR)) {
+			break;
+		}
+	}
+
+	calcEnd(); // restore screen and do stuff
 }
--- hollyhock-2/app_template/main.hpp
+++ hollyhock-2/app_template/main.hpp
@@ -2,21 +2,27 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <sdk/os/gui.hpp>
 
-typedef struct {
+class Chip8 {
+public:
+    Chip8();
+	void loadROM(const char * filename);
+    void cycle();
+
+private:
     uint8_t v[16]; // V0 - VF
-
     uint8_t * mem; // pointer to mem
     size_t mem_size; // 4096
-
     uint8_t * display; // pointer to display
     size_t display_size; // 64x32 pixels
-
     uint16_t stack[16]; 
     uint16_t sp;
-
     uint16_t i; // index register
     uint16_t pc; // program counter
-
     uint8_t delay_timer;
-} chip8_hardware_t;
+
+	void fetch();
+	void decode();
+	void execute();
+};
