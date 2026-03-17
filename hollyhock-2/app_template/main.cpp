1171c388c5a082331d941bc0ee6fda7e05bf6765
friedrichOsDev <f39597054@gmail.com>
2026-03-17 17:22:54 +0000

    move class definitions to main.cpp and fix ROMLoader menu

--- hollyhock-2/app_template/main.cpp
+++ hollyhock-2/app_template/main.cpp
@@ -1,3 +1,5 @@
+#include <stdint.h>
+#include <stddef.h>
 #include <appdef.hpp>
 #include <sdk/calc/calc.hpp>
 #include <sdk/os/lcd.hpp>
@@ -5,104 +7,139 @@
 #include <sdk/os/file.hpp>
 #include <sdk/os/string.hpp>
 #include <sdk/os/debug.hpp>
-#include "main.hpp"
+#include <sdk/os/gui.hpp>
 
 APP_NAME("CHIP-8 Emulator")
 APP_DESCRIPTION("A simple CHIP-8 Emulator")
 APP_AUTHOR("FriedrichOsDev")
 APP_VERSION("1.0.0")
 
-ROMLoader::ROMLoader() : GUIDialog(
-	GUIDialog::Height35,
-	GUIDialog::AlignCenter,
-	"Select ROM",
-	GUIDialog::KeyboardStateNone
-), romMenu(
-	GetLeftX() + 10,
-	GetTopY() + 10,
-	GetRightX() - 10,
-	GetBottomY() - 10,
-	ROM_MENU_EVENT_ID
-), loadBtn(
-	GetLeftX() + 10,
-	GetTopY() + 45,
-	GetLeftX() + 10 + 100,
-	GetBottomY() - 10,
-	"Load",
-	LOAD_BTN_EVENT_ID
-), cancelBtn(
-	GetRightX() - 10 - 100,
-	GetTopY() + 45,
-	GetRightX() - 10,
-	GetBottomY() - 10,
-	"Cancel",
-	CANCEL_BTN_EVENT_ID
-) {
-	selectedROM = 1;
+class ROMLoader : public GUIDialog {
+public:
+    int selectedROM;
 
-	romMenu.AddMenuItem(*(new GUIDropDownMenuItem("Test 1", 1, GUIDropDownMenuItem::FlagEnabled | GUIDropDownMenuItem::FlagTextAlignLeft)));
-	romMenu.AddMenuItem(*(new GUIDropDownMenuItem("Test 2", 2, GUIDropDownMenuItem::FlagEnabled | GUIDropDownMenuItem::FlagTextAlignLeft)));
-	romMenu.AddMenuItem(*(new GUIDropDownMenuItem("Test 3", 3, GUIDropDownMenuItem::FlagEnabled | GUIDropDownMenuItem::FlagTextAlignLeft)));
-	romMenu.AddMenuItem(*(new GUIDropDownMenuItem("Test 4", 4, GUIDropDownMenuItem::FlagEnabled | GUIDropDownMenuItem::FlagTextAlignLeft)));
+    ROMLoader() : GUIDialog(
+		GUIDialog::Height60,
+		GUIDialog::AlignCenter,
+		"Select ROM",
+		GUIDialog::KeyboardStateNone
+	), romMenu(
+		GetLeftX() + 10,
+		GetTopY() + 10,
+		GetRightX() - 10,
+		GetBottomY() - 10 - 30 - 10,
+		ROM_MENU_EVENT_ID
+	), loadBtn(
+		GetLeftX() + 10,
+		GetBottomY() - 10 - 30,
+		GetLeftX() + 10 + 100,
+		GetBottomY() - 10,
+		"Load",
+		LOAD_BTN_EVENT_ID
+	), cancelBtn(
+		GetRightX() - 10 - 100,
+		GetBottomY() - 10 - 30,
+		GetRightX() - 10,
+		GetBottomY() - 10,
+		"Cancel",
+		CANCEL_BTN_EVENT_ID
+	) {
+		selectedROM = 1;
 
-	romMenu.SetScrollBarVisibility(GUIDropDownMenu::ScrollBarVisibleWhenRequired);
+		romMenu.AddMenuItem(*(new GUIDropDownMenuItem("Test 1", 1, GUIDropDownMenuItem::FlagEnabled | GUIDropDownMenuItem::FlagTextAlignLeft)));
+		romMenu.AddMenuItem(*(new GUIDropDownMenuItem("Test 2", 2, GUIDropDownMenuItem::FlagEnabled | GUIDropDownMenuItem::FlagTextAlignLeft)));
+		romMenu.AddMenuItem(*(new GUIDropDownMenuItem("Test 3", 3, GUIDropDownMenuItem::FlagEnabled | GUIDropDownMenuItem::FlagTextAlignLeft)));
+		romMenu.AddMenuItem(*(new GUIDropDownMenuItem("Test 4", 4, GUIDropDownMenuItem::FlagEnabled | GUIDropDownMenuItem::FlagTextAlignLeft)));
 
-	AddElement(romMenu);
-	AddElement(loadBtn);
-	AddElement(cancelBtn);
-}
+		romMenu.SetScrollBarVisibility(GUIDropDownMenu::ScrollBarVisibleWhenRequired);
 
-Chip8::Chip8() {
-	for (int i = 0; i < 16; i++) {
-		v[i] = 0;
+		AddElement(romMenu);
+		AddElement(loadBtn);
+		AddElement(cancelBtn);
 	}
-	
-	mem = (uint8_t *)malloc(4096);
-	mem_size = 4096;
-	memset(mem, 0, mem_size);
-	
-	display = (uint8_t *)malloc(64 * 32);
-	display_size = 64 * 32;
-	memset(display, 0, display_size);
 
-	for (int i = 0; i < 16; i++) {
-		stack[i] = 0;
+    virtual int OnEvent(GUIDialog_Wrapped *dialog, GUIDialog_OnEvent_Data *event) {
+        if (event->GetEventID() == ROM_MENU_EVENT_ID && (event->type & 0xF) == 0xD) {
+            selectedROM = event->data;
+        }
+
+        return GUIDialog::OnEvent(dialog, event);
+    }
+
+private:
+    const uint16_t ROM_MENU_EVENT_ID = 1;
+	GUIDropDownMenu romMenu;
+	const uint16_t LOAD_BTN_EVENT_ID = GUIDialog::DialogResultOK;
+	GUIButton loadBtn;
+	const uint16_t CANCEL_BTN_EVENT_ID = GUIDialog::DialogResultCancel;
+	GUIButton cancelBtn;
+};
+
+class Chip8 {
+public:
+    Chip8() {
+		for (int i = 0; i < 16; i++) {
+			v[i] = 0;
+		}
+		
+		mem = (uint8_t *)malloc(4096);
+		mem_size = 4096;
+		memset(mem, 0, mem_size);
+		
+		display = (uint8_t *)malloc(64 * 32);
+		display_size = 64 * 32;
+		memset(display, 0, display_size);
+
+		for (int i = 0; i < 16; i++) {
+			stack[i] = 0;
+		}
+		
+		sp = 0;
+		i = 0;
+		pc = 0x200;
+		delay_timer = 0;
 	}
-	
-	sp = 0;
-	i = 0;
-	pc = 0x200;
-	delay_timer = 0;
-}
 
-void Chip8::loadROM(const char * filename) {
-	(void)filename;
-}
+	void loadROM(const char * filename) {
+		(void)filename;
+	}
 
-void Chip8::cycle() {
-	fetch();
-	decode();
-	execute();
-}
+    void cycle() {
+		fetch();
+		decode();
+		execute();
+	}
 
-void Chip8::fetch() {
+private:
+    uint8_t v[16]; // V0 - VF
+    uint8_t * mem; // pointer to mem
+    size_t mem_size; // 4096
+    uint8_t * display; // pointer to display
+    size_t display_size; // 64x32 pixels
+    uint16_t stack[16]; 
+    uint16_t sp;
+    uint16_t i; // index register
+    uint16_t pc; // program counter
+    uint8_t delay_timer;
 
-}
+	void fetch() {
+		return;
+	}
 
-void Chip8::decode() {
+	void decode() {
+		return;
+	}
 
-}
-
-void Chip8::execute() {
-
-}
+	void execute() {
+		return;
+	}
+};
 
 extern "C"
 void main() {
 	calcInit(); // backup screen and init some variables
 
 	ROMLoader loader;
-
 	GUIDialog::DialogResult result = loader.ShowDialog();
 
 	if (result != GUIDialog::DialogResultOK) {
@@ -110,7 +147,7 @@ void main() {
 		return;
 	}
 
-	Debug_Printf(10, 10, true, 0, "Selected ROM: %d", loader.selectedROM);
+	Debug_Printf(0, 0, true, 0, "Selected ROM: %d", loader.selectedROM);
 	LCD_Refresh();
 
 	Chip8 chip8;
